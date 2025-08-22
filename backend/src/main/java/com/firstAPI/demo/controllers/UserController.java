112164db8a90bb44bb765c4ee5abc130a5249292
Ayush Sharma <=>
2025-08-22 18:44:28 +0530

    Refactored project structure

--- backend/src/main/java/com/firstAPI/demo/controllers/UserController.java
+++ backend/src/main/java/com/firstAPI/demo/controllers/UserController.java
@@ -6,8 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { Login } from './login/login'
+export const routes: Routes = [
+  { path: 'login', component: Login },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+];
