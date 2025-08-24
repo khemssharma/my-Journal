cd2c763659f70dc70de37d4d694414e8eb5db772
Ayush Sharma <=>
2025-08-24 16:25:00 +0530

    first api-integration

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.scss'
+  styleUrls: ['./login.scss']
 })
 export class Login {
+  userName = '';
+  password = '';
+  errorMessage = '';
+  rememberMe = false;
 
+  constructor(private authService: Auth) {}
+
+  onLogin(): void {
+    this.authService.login(this.userName, this.password).subscribe({
+      next: (token: string) => {
+        console.log('JWT:', token);
+        localStorage.setItem('jwt', token);
+        alert('Login successful!');
+      },
+      error: () => {
+        this.errorMessage = 'Login failed';
+      }
+    });
+  }
 }
--- frontend/src/app/services/auth.ts
+++ frontend/src/app/services/auth.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Auth {
-  
+  private apiUrl = 'http://localhost:8080/public/login';
+
+  constructor(private http: HttpClient) {}
+
+  login(userName: string, password: string): Observable<string> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+
+    const body = { userName, password };
+
+    return this.http.post(this.apiUrl, body, {
+      headers,
+      responseType: 'text'   // 👈 because backend returns plain text (JWT string)
+    });
+  }
 }
